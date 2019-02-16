433b8293367e24d92cc9707deb556979e68c7a04
Oliver Schwarz <oliver.schwarz@gmail.com>
2019-02-16 04:57:47 +0100

    Adding link to the source of the website on github to the menu

--- site/snippets/menu.php
+++ site/snippets/menu.php
@@ -5,6 +5,7 @@
 <?php foreach ($site->children()->listed() as $item): ?>
     <li><?php echo $item->title()->link(); ?></li>
 <?php endforeach; ?>
+    <li><a href="https://github.com/oliverschwarz/oliverschwarz-kirby">Source</a></li>
     <li><a href="https://webbkoll.dataskydd.net/en/results?url=https%3A%2F%2Foliverschwarz.info%2F">Data privacy check</a></li>
   </ul>
 </nav>
