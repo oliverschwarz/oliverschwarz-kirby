da462e788fe4e06c31174debdd2569e41546df25
oliverschwarz <oliver.schwarz@gmail.com>
2019-04-09 13:39:30 +0200

    Removing body element from meta to gain more freedom on single pages

--- site/snippets/meta.php
+++ site/snippets/meta.php
@@ -22,6 +22,3 @@
   <meta property="og:description" content="Oliver is a developer, musician, runner and family man living in the Cologne area in Germany. Join me on Twitter or Strava or see me talking on some events.">
 
 </head>
-<body>
-
-<!-- /meta -->
