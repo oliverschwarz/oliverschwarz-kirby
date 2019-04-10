3e813746deb9db7faa795c2585f2b8d8452abd95
oliverschwarz <oliver.schwarz@gmail.com>
2019-04-10 19:48:08 +0200

    Moving license to the bottom of the page. Extending navigation

--- site/snippets/footer.php
+++ site/snippets/footer.php
@@ -2,9 +2,9 @@
 
     <div class="separator"><img src="<?php echo $site->url(); ?>/assets/images/keine-ahnung-01-c.jpg" width="150px" alt="Separator icon (3 flowers)"></div>
 
-    <p><a rel="license" href="http://creativecommons.org/licenses/by-sa/4.0/">CC-BY-SA 4.0</a>, Oliver Schwarz, <?php echo date('Y'); ?></p>
-
     <?php snippet('menu'); ?>
+
+    <p class="footnote"><a rel="license" href="http://creativecommons.org/licenses/by-sa/4.0/">CC-BY-SA 4.0</a>, Oliver Schwarz, <?php echo date('Y'); ?></p>
 
   </footer>
 
