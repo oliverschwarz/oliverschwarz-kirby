7e22e3e07b33437f628368dc7a68f770a6ff8256
oliverschwarz <oliver.schwarz@gmail.com>
2019-04-10 19:48:56 +0200

    Extending navigation with more items, move standard items to the back of the row

--- site/snippets/menu.php
+++ site/snippets/menu.php
@@ -1,10 +1,10 @@
-<nav class="cf">
+<nav class="nav-footer cf">
   <ul>
     <li><a href="<?php echo $site->url(); ?>">Home</a></li>
 <?php foreach ($site->children()->listed() as $item): ?>
     <li><?php echo $item->title()->link(); ?></li>
 <?php endforeach; ?>
-    <li><a href="https://github.com/oliverschwarz/oliverschwarz-kirby">Source</a></li>
     <li><a href="https://webbkoll.dataskydd.net/en/results?url=https%3A%2F%2Foliverschwarz.info%2F">Data privacy check</a></li>
+    <li><a href="https://github.com/oliverschwarz/oliverschwarz-kirby">Source</a></li>
   </ul>
 </nav>
